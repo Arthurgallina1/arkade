869c6b139d3ac4b2369400ea46eb5910ff64b14d
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-10-19 23:59:47 -0300

    fix charcater

--- src/Character/Character.tsx
+++ src/Character/Character.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import * as S from './styles'
 
+const SIZE = 30
+// map 480x480 / 30 = 16x16
 type CharacterType = {
   x: number
   y: number
 }
 
 export default function Character({ x, y }: CharacterType) {
-  return <S.Container></S.Container>
+  return <S.Container left={x * SIZE} top={x * SIZE}></S.Container>
 }
--- src/Character/styles.tsx
+++ src/Character/styles.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const Container = styled.div<{ left: number; top: number }>`
   width: 30px;
   height: 30px;
   background-color: red;
