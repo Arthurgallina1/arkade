66d010c9309a1e1ab762b17b257641a26123f458
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-10-19 23:57:08 -0300

    add maps, spots and charcter

---
+++ src/App.styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: #24282f;
+  color: white;
+  height: 100vh;
+`
+
+export const Map = styled.div`
+  width: 480px;
+  height: 480px;
+  background-image: url('https://picsum.photos/200');
+  background-position: left top;
+  background-size: 100%;
+`
---
+++ src/Character/Character.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import * as S from './styles'
+
+type CharacterType = {
+  x: number
+  y: number
+}
+
+export default function Character({ x, y }: CharacterType) {
+  return <S.Container></S.Container>
+}
